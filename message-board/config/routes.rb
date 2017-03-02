dd6375c9cd6dc1f464d5effd13263bc1abea183c
takumakatou <amagami0086@gmail.com>
2017-03-02 13:13:15 +0000

    create message model

--- message-board/config/routes.rb
+++ message-board/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   #get 'messages/index', to: 'message#index', as: 'mesages'
+  #root 'messages#index'
   root 'messages#index'
+  resources :messages , only: [:create]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -9,7 +11,7 @@ Rails.application.routes.draw do
   # root 'welcome#index'
 
   # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  #   get 'products/:id'rails generate model Message name:string body:string => 'catalog#view'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
